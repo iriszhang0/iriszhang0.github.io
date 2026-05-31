20a39144664a00f31fc5e3425fac49fedae06bdf
Iris Zhang <iriszhang0010@gmail.com>
2026-05-31 17:44:03 -0400

    Update script.js

--- research/script.js
+++ research/script.js
@@ -55,8 +55,18 @@ function showProjects(projects) {
         <div class="desc">
           <p>${project.desc}</p>
           <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          ${project.links?.view ? `
+        <a href="${project.links.view}" class="btn" target="_blank">
+      <i class="fas fa-eye"></i> View
+    </a>
+  ` : ""}
+
+  ${project.links?.code ? `
+    <a href="${project.links.code}" class="btn" target="_blank">
+      Code <i class="fas fa-code"></i>
+    </a>
+  ` : ""}
+        
           </div>
         </div>
       </div>
